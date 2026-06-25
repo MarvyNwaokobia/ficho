8fa608289c62dc3b5bc7b27a8df175fed0bee734
Marvy <marvynwaokobia@gmail.com>
2026-06-25 08:13:56 +0100

    verify: all 6 contracts verified on Etherscan Sepolia
    - SealedFX: https://sepolia.etherscan.io/address/0x454568f7Fd9bcbD456958C52Fa54A7b8F0384Cf4#code
    - cKES: https://sepolia.etherscan.io/address/0x7333215204c5E37a98BB79a63715593618c0958c#code
    - cNGN: https://sepolia.etherscan.io/address/0x407cD7DD99A10697bA680AB101C86Ca7729D2f74#code
    - KES: https://sepolia.etherscan.io/address/0xA08Bc6EDd1A09500Dea6bc810A8fCE24a458B617#code
    - NGN: https://sepolia.etherscan.io/address/0x5f121712C0dBE853b9B079BE25100e0604AA7AcF#code
    - USDT: https://sepolia.etherscan.io/address/0xe61b662C0e2C0855A9d14E8fF2BF1f5065F072A7#code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -20,9 +20,7 @@ const config: HardhatUserConfig = {
     deployer: 0,
   },
   etherscan: {
-    apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY || "",
-    },
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
   },
   gasReporter: {
     currency: "USD",
